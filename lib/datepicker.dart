5a2b4c494e1b71f5bd5e30dbcaebbf1718d002f4
Samip Nepal <samipnepal5@gmail.com>
2026-03-18 17:51:55 +0545

    Adding dependecies intl for datafromating

--- lib/datepicker.dart
+++ lib/datepicker.dart
@@ -1,5 +1,6 @@
 // ignore: depend_on_referenced_packages
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 class Datepicker extends StatefulWidget {
@@ -10,48 +11,45 @@ class Datepicker extends StatefulWidget {
 }
 
 class _DatepickerState extends State<Datepicker> {
+  String _selectedDate = '';
   String _selectedDateRange = '';
-
-  void _onselected(DateRangePickerSelectionChangedArgs ags) {
-    setState(() {
-      if (ags.value is PickerDateRange) {
-        _selectedDateRange = '${ags.value.startDate} - ${ags.value.endDate}';
-      } else if (ags.value is DateTime) {
-        _selectedDateRange = ags.value.toString();
-      } else if (ags.value is List<DateTime>) {
-        _selectedDateRange = ags.value.toString();
-      } else {
-        _selectedDateRange = '';
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Datepicker'),
-          backgroundColor: Color.fromARGB(255, 20, 220, 97),
+          title: Text("Datepicker"),
+          backgroundColor: Color.fromARGB(255, 176, 223, 194),
         ),
         body: Center(
           child: Column(
             children: [
+              Text("Selected date single date"),
               SizedBox(
-                  height: 400,
-                  width: 400,
-                  child: SfDateRangePicker(
-                    onSelectionChanged: _onselected,
-                    backgroundColor: const Color.fromARGB(255, 127, 193, 145),
-                    todayHighlightColor: Colors.red,
-                    selectionMode: DateRangePickerSelectionMode.range,
-                  )),
-              FittedBox(
-                child: SizedBox(
-                  height: 100,
-                  width: 300,
-                  child: Text('Selected date range: $_selectedDateRange'),
+                height: 200,
+                width: 300,
+                child: SfDateRangePicker(
+                  onSelectionChanged: (args) {
+                    setState(() => _selectedDate =
+                        DateFormat('d MMMM y ').format(args.value));
+                  },
                 ),
-              )
+              ),
+              Text('Selected date: $_selectedDate'),
+              Divider(),
+              Text("Selected date range"),
+              SizedBox(
+                height: 200,
+                width: 300,
+                child: SfDateRangePicker(
+                    selectionMode: DateRangePickerSelectionMode.range,
+                    onSelectionChanged: (args) {
+                      setState(() {
+                        _selectedDateRange =
+                            '${DateFormat('d MMMM y').format(args.value.startDate)} - ${DateFormat('d MMMM y').format(args.value.endDate)}';
+                      });
+                    }),
+              ),
+              Text('Selected date range: $_selectedDateRange'),
             ],
           ),
         ));
